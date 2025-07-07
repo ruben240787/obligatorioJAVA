911828c3705fe44d6631c6c45c1a9124270d4449
ruben240787 <rubendnl24@gmail.com>
2025-07-06 21:33:04 -0300

    add

--- Camping/src/main/java/com/mycompany/camping/verCamping.java
+++ Camping/src/main/java/com/mycompany/camping/verCamping.java
@@ -6,13 +6,14 @@ import java.util.Scanner;
 public class verCamping {
 
     public static void main(String[] args) {
+        //Escaner para ingresar los datos.
         Scanner scanner = new Scanner(System.in);
-        
+        // Hardcodeamos las cabañas.
         Cabania [] cabanias = { new Cabania ('a',"06/07/25", "07/07/25", 10,5,5000)};
 
         int cantPersonas = 0;
         boolean entradaValida = false;
-        
+        // solicitamos la cantidad de dias de estadia.
         System.out.print("Ingrese la cantidad de dias a alojarse: ");
         cabanias[1].setDiasEstadia(scanner.nextInt());
 
