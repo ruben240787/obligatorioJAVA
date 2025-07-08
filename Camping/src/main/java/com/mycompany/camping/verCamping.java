4aa6ed8bd44cd267746731a030d413555c8b90f9
ruben240787 <rubendnl24@gmail.com>
2025-07-07 23:10:25 -0300

    cambios

--- Camping/src/main/java/com/mycompany/camping/verCamping.java
+++ Camping/src/main/java/com/mycompany/camping/verCamping.java
@@ -9,15 +9,35 @@ public class verCamping {
         //Escaner para ingresar los datos.
         Scanner scanner = new Scanner(System.in);
         // Hardcodeamos las cabañas.
-        Cabania [] cabanias = { new Cabania ('a',"06/07/25", "07/07/25", 10,5,5000)};
+        Cabania [] cabanias = { new Cabania ('a',"", "", 10,5,5000)};
         
         // Clase Persona.
         TitularResponsable cliente1 = new TitularResponsable ("", "0","0", true, 0);
+        
+        
+        //Inicio del Programa
+        System.out.println("------------------------------");
+        System.out.println("----ADMINISTRACION CAMPING----");
+        System.out.println("------------------------------");
+        System.out.println("");
+        System.out.println("");
+        
+        
+        System.out.print("Ingrese Nombre del cliente");
         cliente1.setNombreCompleto(scanner.nextLine());
-        cliente1.numeroDoc(scanner.nextLine());
-        cliente1.numeroTarjeta(scanner.nextLine());
-        cliente1.numeroTelefono(scanner.nextLine());
+        System.out.print("Ingrese Fecha de Ingreso   ej: 01/01/25");
+        cabanias[0].setEntrada(scanner.nextLine());
+        System.out.print("Ingrese Fecha estimada de Salida  ej: 01/01/25");
+        cabanias[0].setSalida(scanner.nextLine());        
+        System.out.print("Ingrese numero de documento del cliente");
+        cliente1.setNumeroDoc(scanner.nextLine());
+        System.out.print("Ingrese ingrese numero de tarjeta");
+        cliente1.setNumeroTarjeta(scanner.nextInt());
+        System.out.print("Ingrese numero de telefono");
+        cliente1.setNumeroTelefono(scanner.nextLine());
+        
         int cantPersonas = 0;
+        
         boolean entradaValida = false;
         // solicitamos la cantidad de dias de estadia.
         System.out.print("Ingrese la cantidad de dias a alojarse: ");
@@ -41,5 +61,6 @@ public class verCamping {
         //Llamamos a la funcion cobroEstadia para mostar por pantalla el monto total a cobrar.
         int total = cabanias[1].cobroEstadia(cantPersonas);
         System.out.println("El total a pagar por la estadía es: $" + total + " en la cabaña " + cabanias[1].nombre);
+        System.out.println("Datos del Responsable " + cliente1);
     }
 }
